b31bcc3eacb9b997fa214f6cbddf7425f69ff643
Nima <nima.ebrazi1373@gmail.com>
2019-03-26 18:57:04 +0430

    ServiceProvider added

---
+++ src/ApiPresenterServiceProvider.php
@@ -0,0 +1,71 @@
+<?php
+
+
+namespace LaravelApiPresenter;
+
+
+use Illuminate\Cache\Repository as CacheRepository;
+use Illuminate\Support\ServiceProvider;
+use LaravelApiPresenter\Contract\ApiPresenterInterface;
+use LaravelApiPresenter\Contract\ResponseInterface;
+use LaravelApiPresenter\Factory\JsonResponseFactory;
+use LaravelApiPresenter\Factory\ResponseFactory;
+use LaravelApiPresenter\Presenter\JsonApiPresenter;
+use LaravelApiPresenter\Presenter\Response;
+
+class ApiPresenterServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = true;
+
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton(ResponseInterface::class, function () {
+            return new Response();
+        });
+
+
+        $this->registerJsonImplementation();
+    }
+
+    protected function registerJsonImplementation()
+    {
+        $this->app->singleton(ResponseFactory::class, function () {
+            return new JsonResponseFactory();
+        });
+
+        $this->app->singleton(ApiPresenterInterface::class, function () {
+
+            return new JsonApiPresenter(
+                $this->app->make(ResponseInterface::class),
+                $this->app->make(ResponseFactory::class),
+                $this->app->make(CacheRepository::class)
+            );
+
+        });
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [
+            ResponseFactory::class,
+            ResponseInterface::class,
+            ApiPresenterInterface::class,
+        ];
+    }
+}
