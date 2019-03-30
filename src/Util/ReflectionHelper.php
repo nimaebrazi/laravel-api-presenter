39bd290cddc887cd9b17a63c15c2eb1b15996b65
Nima <nima.ebrazi1373@gmail.com>
2019-03-30 18:32:21 +0430

    reflection helper added

---
+++ src/Util/ReflectionHelper.php
@@ -0,0 +1,47 @@
+<?php
+
+
+namespace LaravelApiPresenter\Util;
+
+
+class ReflectionHelper
+{
+    /**
+     * @param $object
+     *
+     * @return array
+     */
+    public static function getProProperties($object)
+    {
+        $class = new \ReflectionClass($object);
+        $properties = $class->getProperties();
+
+        return self::getArrayOfProperties($object, $properties);
+
+    }
+
+    /**
+     * @param $object
+     * @param $properties
+     * @return array
+     */
+    protected static function getArrayOfProperties($object, $properties): array
+    {
+        $all = [];
+
+        /** @var \ReflectionProperty  $property */
+        foreach ($properties as $property) {
+
+            $property->setAccessible(true);
+
+            $propertyValue = $property->getValue($object);
+            if (!empty($propertyValue) || !is_null($propertyValue)) {
+
+                $snakeCaseProProperty = \Str::snake($property->getName());
+                $all[$snakeCaseProProperty] = $property->getValue($object);
+            }
+        }
+
+        return $all;
+    }
+}
